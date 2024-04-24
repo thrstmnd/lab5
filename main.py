750c0a51d383eb0ccb8306a6a7871285b89d3f06
thrstmnd <160107385+thrstmnd@users.noreply.github.com>
2024-04-25 01:52:42 +0300

    main.py

--- main.py
+++ main.py
@@ -1,45 +1,48 @@
+import random
 def zadacha1():
-    s = [1, 2, 3, 4, 5]
+    s = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
     x =int(input('Введите число'))
     if x in s:
-        print('Вы угадали')
+        print('Поздравляю, Вы угадали число!')
     else:
-        print('Вы не угадали')
+        print('Нет такого числа!')
 
 def zadacha2():
-    d = []
-    s = [1 ,3 ,5 ,3 ,5 ,2 ,6]
-    for x in s:
-        if s.count(x) > 1:
-            if x not in d:
-                d.append(x)
-    print(*d)
-
+    s1 = []
+    s = [1 ,2 , 3, 4, 5, 5]
+    for i in s:
+        if s.count(i) > 1:
+            if i not in s1:
+                s1.append(i)
+    print(*s1)
 
 def zadacha3():
-    days = ('Пн','Вт','Ср','Чт','Пт' ,'Сб','Вс')
-    s = int(input('Введите количество выходных'))
-    for i in days:
-        print('Рабочие', *days[:-s])
-        print('Выходные', *days[-s:])
-        break
+    day = ('Понедельник','Вторник','Среда','Четверг','Пятница' ,'Суббота','Воскресенье')
+    s = int(input('Количество выходных?'))
+    print('Ваши выходные дни:', *day[-s:])
+    print('Ваши рабочие дни:', *day[:-s])
 
 def zadacha4():
-    import random
-    gr1=('Аб','Дн','Ке','Си','Ле','Жи','Ве','Ба','Гу','Зе')
-    gr2=('Еп', 'Уз','Оп','Ан','Ид','Ык','Эо','Юс','Яр','Ой')
+    group1=('иванов','соколов','семенов','сидоров','попов','романов','сергеев','пав','ывв','ыфвф')
+    group2=('ячс', 'жжо','трр','иим','ххз','йц','нн','рпа','авывы','аыв')
+    #a
     a1=[]
+    a1.append(random.sample(group1,5))
     a2=[]
+    a2.append(random.sample(group2,5))
     a=[]
-    a1.append(random.sample(gr1,5))
-    a2.append(random.sample(gr2,5))
     a.extend(*a1)
     a.extend(*a2)
     a=tuple(a)
-    print('a', *gr1)
-    print('a', *gr2)
-    print('a', *a)
-    print('b', len(a))
-    print('d', *sorted(a))
-    print('e', 'Иванов' in a)
-    print('e', a.count('Иванов)'))
+    #b
+    print(group1)
+    print(group2)
+    print(*a)
+    #c
+    print(len(a))
+    #d
+    print(*sorted(a))
+    #e
+    print('иванов' in a , a.count('иванов')))
+
+zadacha1()
